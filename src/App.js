e97346836c259fb32742c4dc5e7a77234cc84c37
victor-singha <vicunite2@gmail.com>
2021-05-01 11:28:53 +0530

    [victor] Stat Component added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Stats from './components/Stats';
 import Team from './components/Team'
 
 function App() {
   return (
     <div className="App">
       <Team/>
+      <Stats/>
     </div>
   );
 }
