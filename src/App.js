29a92d167fb0d61850c706d9229085c1e2edefb9
victor-singha <vicunite2@gmail.com>
2021-05-01 12:17:56 +0530

    [victor] Hero.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Footer from './components/Footer';
 import Gyan from './components/Gyan';
+import Hero from './components/Hero';
 import Stats from './components/Stats';
 import Team from './components/Team'
 
 function App() {
   return (
     <div className="App">
+      <Hero/>
       <Stats/>
       <Team/> 
       <Gyan/> 
