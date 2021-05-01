ecbf58ef7a5f485681937a48fbe94d90afcf86e4
victor-singha <vicunite2@gmail.com>
2021-05-01 11:39:22 +0530

    [victor] Gyan.js and Footer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
+import Footer from './components/Footer';
+import Gyan from './components/Gyan';
 import Stats from './components/Stats';
 import Team from './components/Team'
 
 function App() {
   return (
     <div className="App">
-      <Team/>
       <Stats/>
+      <Team/> 
+      <Gyan/> 
+      <Footer/>
     </div>
   );
 }
